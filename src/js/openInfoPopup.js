f028abfe8e2d97e7d560d479facc9315b82d799a
Andrea de Martinis <87342859+AndreadeMartinis@users.noreply.github.com>
2023-11-28 19:22:07 +0100

    Quarto commit

--- src/js/openInfoPopup.js
+++ src/js/openInfoPopup.js
@@ -2,7 +2,7 @@ export function openInfoPopup() {
   const popupImage = document.querySelector(".popupImage");
   const overlayPopup = document.querySelector(".overlay-popup");
 
-  popupImage.src = "src/img/infoPromo.png";
+  popupImage.src = "src/img/infoPromo.jpg";
   popupImage.alt = "Info Evento";
   overlayPopup.style.display = "flex";
 
